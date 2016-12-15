23bd69370f1498e9d9d6193d6d950a43be251ede
Victor <victorgarcia.esp@gmail.com>
2016-12-15 18:11:50 +0100

    Casos test de Sulfuras y clase Sulfuras

---
+++ CasosSulfura.py
@@ -0,0 +1,9 @@
+
+from clases import *
+
+if __name__ == '__main__':
+
+    item = Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80)
+    # chequeo herencia __repr__
+    print(item)
+    # test update_quality
--- sulfuras.py
+++ sulfuras.py
@@ -1,7 +1,9 @@
 from clases import *
-from casos_test.txt import *
+from sulfuras import *
 
 class Sulfuras(NormalItem):
-
-    self.quality = 80
-    self.sellIn = 0
+    def __init__(self, name, sell_in, quality):
+        NormalItem.__init__(self, name, sell_in, quality)
+    def update_quality(self):
+            self.quality = 80
+            self.sellIn = 0
